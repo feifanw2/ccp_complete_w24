657a08c74418bf81cae059e9f3d15fccf9a9294b
feifanw2 <157077696+feifanw2@users.noreply.github.com>
2024-04-09 09:11:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+    var img = document.getElementById('myImage');
+    img.style.opacity = 1; 
+});
+
+window.addEventListener('load', function() {
+    var img = document.getElementById('myImage_1');
+    img.style.opacity = 1; 
+});
+
+window.addEventListener('load', function() {
+    var img = document.getElementById('myImage_2');
+    img.style.opacity = 1; 
+});
